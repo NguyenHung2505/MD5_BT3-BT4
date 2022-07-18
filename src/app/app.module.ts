f7c67e020eeae20f6f0aa3ace940f1c074fd18ae
chandoithatsu <100838893+Afdfghjhgcfghvh@users.noreply.github.com>
2022-07-18 15:01:23 +0700

    ok

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,35 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FontSizeEditorComponent } from './font-size-editor/font-size-editor.component';
+import {FormsModule} from '@angular/forms';
+import { PetComponent } from './pet/pet.component';
+import { ProductComponent } from './product/product.component';
+import { ArticleComponent } from './article/article.component';
+import { LikeComponent } from './like/like.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { AdminComponent } from './admin/admin.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FontSizeEditorComponent,
+    PetComponent,
+    ProductComponent,
+    ArticleComponent,
+    LikeComponent,
+    NavbarComponent,
+    AdminComponent,
+    HomeComponent,
+    LoginComponent
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        FormsModule
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
---
+++ test/chaytest.js
@@ -0,0 +1,9 @@
+
+function login() {
+  // let b = "login"
+  document.getElementById("hien").innerHTML= "login"
+}
+
+function loginAdmin() {
+  document.getElementById("hien").innerHTML = "login admin"
+}
